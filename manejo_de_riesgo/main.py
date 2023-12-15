1db7b7f915319772f9c41f6c756a4f3869fa95a3
onnti <nicolasbaseggiodev@gmail.com>
2023-12-14 23:20:44 -0300

    commmit

--- manejo_de_riesgo/main.py
+++ manejo_de_riesgo/main.py
@@ -1,24 +1,60 @@
 from datetime import date
-from data_procesing.f_yfinance import YFinanceDataFetcher
-from factory.port_factory import PortfolioOptimizer
-from analizer.port_analizer_own import calculate_portfolio_returns
-from analizer.port_analizer_qs import generate_report
-
-assets = ['JPM', 'MSFT', 'BA', 'KO', 'AAPL', 'TSLA', 'AMZN', 'NVDA']
-train_start = '2019-01-01'
-train_end = '2022-12-30'
-test_start = '2022-12-30'
-test_end = str(date.today())
-
-portfolio_w = PortfolioOptimizer().port_optimize(YFinanceDataFetcher()
-                                                 .get_training_data(assets, train_start, train_end))
-
-portfolio = calculate_portfolio_returns(YFinanceDataFetcher()
-                                        .get_testing_data(assets, test_start, test_end), portfolio_w.T)
-
-stock = portfolio['Daily Return']
-report = generate_report(stock)
 
 
-if __name__ == "__main__":
-    print(report)
+from portfolio.data_procesing.f_yfinance import YFinanceDataFetcher
+from portfolio.factory.port_hc_factory import PortfolioOptimizer
+from portfolio.analizer.port_analizer_own import calculate_portfolio_returns
+from portfolio.analizer.port_analizer_qs import generate_report
+from models import PortfolioUpdate
+from fastapi import FastAPI, HTTPException
+from exception import (YFinanceDataFetchError, PortfolioOptimizationError, TestingDataFetchError,
+                       PortfolioReturnsCalculationError)
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return 'Welcome to the portfolio API!'
+
+#hola
+@app.put("/portfolio/update")
+def update_portfolio(update_data: PortfolioUpdate):
+    assets = update_data.assets
+    train_start = update_data.train_start
+    train_end = update_data.train_end
+    test_start = update_data.test_start
+    test_end = str(date.today())
+
+    try:
+        stock = Portfolio(assets, train_start, train_end, test_start, test_end).update_portfolio()
+        report = generate_report(stock)
+        return report
+    except YFinanceDataFetchError as e:
+        raise HTTPException(status_code=500, detail=f'Error fetching data: {str(e)}')
+
+
+class Portfolio:
+    def __init__(self, assets, train_start, train_end, test_start, test_end):
+        self.assets = assets
+        self.train_start = train_start
+        self.train_end = train_end
+        self.test_start = test_start
+        self.test_end = test_end
+
+    def update_portfolio(self):
+        try:
+            training_data = YFinanceDataFetcher().get_training_data(self.assets, self.train_start, self.train_end)
+            portfolio_w = PortfolioOptimizer().port_optimize(training_data)
+            testing_data = YFinanceDataFetcher().get_testing_data(self.assets, self.test_start, self.test_end)
+            portfolio_returns = calculate_portfolio_returns(testing_data, portfolio_w.T)
+            return portfolio_returns['Daily Return']
+        except YFinanceDataFetchError as yfinance_error:
+            return f'Error fetching financial data: {str(yfinance_error)}'
+        except PortfolioOptimizationError as optimization_error:
+            return f'Error in portfolio optimization: {str(optimization_error)}'
+        except TestingDataFetchError as testing_error:
+            return f'Error fetching testing data: {str(testing_error)}'
+        except PortfolioReturnsCalculationError as returns_error:
+            return f'Error calculating portfolio returns: {str(returns_error)}'
+        except Exception as other_error:
+            return f'Unexpected error: {str(other_error)}'
